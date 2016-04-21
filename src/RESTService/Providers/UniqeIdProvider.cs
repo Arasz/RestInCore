655065504db0f38c4056e078d116c05f561df9d4
AraszDev <rafal.araszkiewicz@student.put.poznan.pl>
2016-04-21 23:34:14 +0200

    Add new files to project
    Two new controllers were added, one for marks and second for subjects.
    Also there is UniqueIdProvider class which provides unique id for
    students. Entity updater updates all properties in in given entity with
    new state without changing entity refrence. This class will be deleted

---
+++ src/RESTService/Providers/UniqeIdProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace RESTService.Providers
+{
+    /// <summary>
+    /// Provides unique id number 
+    /// </summary>
+    [DataContract]
+    public class UniqueIdProvider
+    {
+        [DataMember]
+        private int _lastId = 0;
+
+        public int Id => _lastId++;
+    }
+}
---
+++ src/RESTService/Utils/EntityUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace RESTService.Utils
+{
+    /// <summary>
+    /// Updates entity state with new state 
+    /// </summary>
+    /// <typeparam name="T"> Entity type </typeparam>
+    public class EntityUpdater<T> where T : class
+    {
+        private readonly PropertyInfo[] _allEntityProperties;
+        private readonly T _entity;
+
+        public EntityUpdater(T entity)
+        {
+            _entity = entity;
+
+            _allEntityProperties = _entity.GetType().GetProperties();
+        }
+
+        /// <summary>
+        /// Updates all properties of entity with new values from new entity 
+        /// </summary>
+        /// <param name="newEntity"> Entity from which new values will be taken </param>
+        public void Update(T newEntity)
+        {
+            foreach (var propertyInfo in _allEntityProperties)
+            {
+                propertyInfo.SetValue(_entity, propertyInfo.GetValue(newEntity));
+            }
+        }
+    }
+}
