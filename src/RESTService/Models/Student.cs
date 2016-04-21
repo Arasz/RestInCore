4ae996964221c8dcd8d53afd407f8eeb677b0264
AraszDev <rafal.araszkiewicz@student.put.poznan.pl>
2016-04-21 11:54:01 +0200

    Mark Student class as serializable

--- src/RESTService/Models/Student.cs
+++ src/RESTService/Models/Student.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace RESTService.Models
 {
     /// <summary>
     /// Student data model 
     /// </summary>
+    [DataContract]
     public class Student
     {
+        [DataMember]
         public DateTime Birthday { get; set; } = DateTime.MinValue;
 
+        [DataMember]
         public string Name { get; set; } = "";
 
+        [DataMember]
         public string Surname { get; set; } = "";
 
         public override string ToString()
