fac20e7debea4a5afe06e0c8f06a4ac19864f271
AraszDev <rafal.araszkiewicz@student.put.poznan.pl>
2016-04-21 11:59:08 +0200

    Mark rest data model classes as serializable

--- src/RESTService/Models/StudentMark.cs
+++ src/RESTService/Models/StudentMark.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace RESTService.Models
 {
     /// <summary>
-    /// Student mark data model
+    /// Student mark data model 
     /// </summary>
+    [DataContract]
     public class StudentMark
     {
+        [DataMember]
         public Student Student { get; set; }
 
+        [DataMember]
         public DateTime SubmitTime { get; set; } = DateTime.Now;
 
+        [DataMember]
         public Mark Value { get; set; } = Mark.Two;
     }
 
--- src/RESTService/Models/Subject.cs
+++ src/RESTService/Models/Subject.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace RESTService.Models
 {
     /// <summary>
     /// Subject data model 
     /// </summary>
+    [DataContract]
     public class Subject
     {
+        [DataMember]
         public IEnumerable<StudentMark> Marks { get; set; } = new List<StudentMark>();
 
+        [DataMember]
         public string Name { get; set; } = "";
 
+        [DataMember]
         public string Teacher { get; set; } = "";
     }
 }
