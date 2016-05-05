ce6cf398022ee70ee50491c493879ab25e9f7c6b
Arasz <rafal.araszkiewicz@student.poznan.pl>
2016-05-05 20:03:54 +0200

    Change mongoDB driver

--- src/RESTService/Startup.cs
+++ src/RESTService/Startup.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Newtonsoft.Json.Serialization;
 using RESTService.Models;
 using RESTService.Providers;
@@ -42,11 +44,10 @@ namespace RESTService
         }
 
         // This method gets called by the runtime. Use this method to add services to the container
-        public void ConfigureServices(IServiceCollection services)
+        // We can change default IoC container for our custom container
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-
-            services.AddApplicationInsightsTelemetry(Configuration);
 
             var mvc = services.AddMvc(configuration =>
            {
@@ -62,6 +63,14 @@ namespace RESTService
 
             services.AddSingleton<IRepository<Entity>, Repository.Repository>();
             services.AddSingleton<IIdentityProvider<int>, UniqueIdentityProvider>();
+
+            //MongoDB client initialization
+            var mongoClient = new MongoClient(new MongoClientSettings()) { Settings = { Server = new MongoServerAddress("localhost",8004)}};
+            services.AddInstance<IMongoClient>(mongoClient);
+            services.AddInstance(mongoClient.GetDatabase("universityBase"));
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
         }
     }
 }
